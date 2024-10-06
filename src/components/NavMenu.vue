4bd52cda30b65b588a962b37b3de705b45800b37
Andres Olivares <spartan_abg@hotmail.com>
2024-10-06 12:18:54 -0500

    Finished project

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -5,7 +5,7 @@
     <RouterLink :to="{name: 'SettingsPage'}">Settings</RouterLink>
 
     <div v-if="isAuthenticated">
-      Welcome {{ user.email }}
+      Welcome: {{ user.email }}
       <button @click="logout" class="rounded-md bg-red-500 px-4 py-2 mx-2 text-red-100 hover:bg-red-700">LOGOUT</button>
     </div>
     <div v-else>
--- src/router.js
+++ src/router.js
@@ -9,11 +9,11 @@ import { useAuth } from './composables/useAuth'
 const {isAuthenticated} = useAuth()
 
 const routes = [
-  { path: '/', name: 'Home', component: HomePage },
-  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
-  { path: '/food/:id', name: 'CardDetails', component: CardDetails },
-  { path: '/login', name: 'LoginPage', component: LoginPage },
-  { path: '/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
+  { path: '/project5', name: 'Home', component: HomePage },
+  { path: '/project5/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
+  { path: '/project5/food/:id', name: 'CardDetails', component: CardDetails },
+  { path: '/project5/login', name: 'LoginPage', component: LoginPage },
+  { path: '/project5/settings', name: 'SettingsPage', component: SettingsPage, meta: {requiresAuth: true} },
 
 ]
 
